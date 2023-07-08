5b3f988cdd9ec0998b43726f266eccd9e77ebb0c
insyria <insyria@gmail.com>
2023-07-07 21:34:02 -0400

    Change URL

--- site/next.config.js
+++ site/next.config.js
@@ -2,11 +2,11 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'Bahr Images',
+    description: 'Personal images.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://insyria.github.io/kasm-registry/',
+    contactUrl: 'https://github.com/insyria/kasm-registry/issues',
   },
   reactStrictMode: true,
   swcMinify: true,
